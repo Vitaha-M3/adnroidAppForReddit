ca3e50ed8b47f41306fee58801dc8cc200124985
Vitalii Drobot <vitalii_drobot@epam.com>
2023-01-25 19:10:13 +0200

    Implemented function saving images

--- app/src/main/java/com/example/appforreddit/ImageFullscreenActivity.java
+++ app/src/main/java/com/example/appforreddit/ImageFullscreenActivity.java
@@ -1,20 +1,61 @@
 package com.example.appforreddit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.squareup.picasso.Picasso;
 
 public class ImageFullscreenActivity extends AppCompatActivity {
+    private static final int IDM_SAVE = 101;
+    private ImageView imageFullscreenView;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_fullscreen);
 
-        ImageView imageFullscreenView = findViewById(R.id.imageFullscreenView);
+        imageFullscreenView = findViewById(R.id.imageFullscreenView);
         String imageURL = (String) getIntent().getExtras().get("image");
         Picasso.get().load(imageURL).into(imageFullscreenView);
+
+        registerForContextMenu(imageFullscreenView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(ContextMenu.NONE, IDM_SAVE, ContextMenu.NONE,"Save image");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        CharSequence message;
+        if (item.getItemId() == IDM_SAVE){
+            saveImage(imageFullscreenView);
+            message = "Image saved!";
+        }else
+            return super.onContextItemSelected(item);
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+        return true;
+    }
+
+    private void saveImage(ImageView targetImage){
+        Bitmap bitmap = ((BitmapDrawable)targetImage.getDrawable()).getBitmap();
+        MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "image1", "image from Reddit");
     }
 }
